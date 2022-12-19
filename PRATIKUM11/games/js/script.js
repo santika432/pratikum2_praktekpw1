433f359685b140b7fdb8c42c3049c38a04aa4715
santika432 <114549409+santika432@users.noreply.github.com>
2022-12-19 17:37:50 +0700

    praktikum 11

---
+++ PRATIKUM11/games/js/script.js
@@ -0,0 +1,43 @@
+const result = document.querySelector("#result");
+
+// menangkap semua elemen a
+document.querySelectorAll("#option a").forEach((a) =>
+    // jika di klick akan menjalankan fungsi computerChoice
+    a.addEventListener("click", (e) => {
+        computerChoice(e);
+    })
+);
+
+
+// fungsi untuk menghandle logic pada game 
+function computerChoice(e) {
+    let choices = ["Rock", "Paper", "Scissors"];
+
+    // pilihan random computer
+    result.innerHTML = choices[Math.floor(Math.random() * choices.length)];
+
+    // jika pilihan komputer == dengan pilihan user (draw)
+    if (e.target.innerText.trim() == result.innerHTML) {
+        setTimeout(() => alert("Draw"), 100);
+    }
+
+
+    // jika pilihan user yg menang
+    if (
+        (e.target.innerText.trim() == "Rock" && result.innerHTML == "Scissors") ||
+        (e.target.innerText.trim() == "Paper" && result.innerHTML == "Rock") ||
+        (e.target.innerText.trim() == "Scissors" && result.innerHTML == "Paper")
+    ) {
+        setTimeout(() => alert("You win"), 100);
+    }
+
+
+    // jika pilihan komputer yg menang
+    if (
+        (e.target.innerText.trim() == "Rock" && result.innerHTML == "Paper") ||
+        (e.target.innerText.trim() == "Paper" && result.innerHTML == "Scissors") ||
+        (e.target.innerText.trim() == "Scissors" && result.innerHTML == "Rock")
+    ) {
+        setTimeout(() => alert("Computer wins"), 100);
+    }
+}
